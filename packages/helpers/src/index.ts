745945129efbe7bae65bf617a4f94cbd5e2cde7d
esoto76 <elpasocolorartist@yahoo.com>
2022-08-21 16:31:22 -0600

    feat(helpers): add IsDef

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,2 +1,3 @@
+export * from './is-fns';
 export * from './pkg';
 export * from './types';
---
+++ packages/helpers/src/is-fns.ts
@@ -0,0 +1,3 @@
+import { ISFN } from './types';
+
+export const IsDef: ISFN = v => (v !== undefined ? true : false);
