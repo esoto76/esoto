86ab344d3a8a7e6b7961bcd23e9b767732904090
esoto76 <elpasocolorartist@yahoo.com>
2022-08-21 17:11:00 -0600

    feat(helpers): add MergeObjs

--- packages/helpers/src/obj.ts
+++ packages/helpers/src/obj.ts
@@ -1,6 +1,6 @@
 import { IsDef } from './misc';
 import { IsArr, IsArrDef } from './arr';
-import { ISFN } from './types';
+import { ISFN, GEN_OBJ } from './types';
 
 export const IsObj: ISFN = v =>
   IsDef(v) && !IsArr(v) && typeof v === 'object' ? true : false;
@@ -10,3 +10,14 @@ export const OKeys: (v: any) => string[] = v =>
 
 export const IsObjDef: ISFN = v =>
   IsObj(v) && IsArrDef(OKeys(v)) ? true : false;
+
+export const MergeObjs: (v: GEN_OBJ[]) => GEN_OBJ = v => {
+  if (IsObj(v)) return v;
+  if (!IsArrDef(v)) return {};
+
+  const obj = {};
+
+  for (let o of v) if (IsObjDef(o)) for (let k in o) obj[k] = o[k];
+
+  return obj;
+};
