32fa5b4c879a0bc687a07ee8467fb7788e44c2a3
esoto76 <elpasocolorartist@yahoo.com>
2022-08-22 14:03:38 -0600

    feat(helpers): add NormalizePath

--- packages/helpers/src/string.ts
+++ packages/helpers/src/string.ts
@@ -1,7 +1,11 @@
-import { ISFN } from './types';
-import { IsDef, IsLength } from './misc';
+import { ISFN } from "./types";
+import { IsDef, IsLength } from "./misc";
+import { sep } from "path";
 
 export const IsStr: ISFN = v =>
-  IsDef(v) && typeof v === 'string' ? true : false;
+  IsDef(v) && typeof v === "string" ? true : false;
 
 export const IsStrDef: ISFN = v => (IsStr(v) && IsLength(v) ? true : false);
+
+export const NormalizePath: (str: string) => string = str =>
+  IsStrDef(str) ? str.split(sep).join("/") : "";
