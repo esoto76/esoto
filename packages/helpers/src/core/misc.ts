4cd207e98dbd2937b5baf9c9cfb30cbed3d56de9
esoto76 <elpasocolorartist@yahoo.com>
2022-08-22 16:59:18 -0600

    feat(helpers): add GetTypeOf and IsTypeOf

--- packages/helpers/src/core/misc.ts
+++ packages/helpers/src/core/misc.ts
@@ -1,6 +1,13 @@
-import { ISFN } from './types';
+import { ISFN } from "./types";
+import { IsArr } from "./arr";
 
 export const IsDef: ISFN = v => (v !== undefined ? true : false);
 
 export const IsLength: ISFN = v =>
   IsDef(v) && IsDef(v.length) && v.length > 0 ? true : false;
+
+export const GetTypeOf: (v: any) => string = v =>
+  IsDef(v) ? (IsArr(v) ? "array" : typeof v) : "";
+
+export const IsTypeOf: (x: any, y: any) => boolean = (x, y) =>
+  IsDef(x) && IsDef(y) && GetTypeOf(x) === GetTypeOf(y) ? true : false;
