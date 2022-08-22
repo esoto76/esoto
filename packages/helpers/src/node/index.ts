2e19e1a3cd4cc281647a4b20b3b705f3e12e2b04
esoto76 <elpasocolorartist@yahoo.com>
2022-08-22 15:15:48 -0600

    feat(helpers): add IsPkg and GetPkg

--- packages/helpers/src/node/index.ts
+++ packages/helpers/src/node/index.ts
@@ -1,1 +1,10 @@
-export const NName = "node";
+import { ISFN, IsStrDef } from "../core";
+import { existsSync } from "fs";
+
+export const IsPkg: ISFN = v =>
+  IsStrDef(v) && existsSync(v) && existsSync(`${v}/package.json`)
+    ? true
+    : false;
+
+export const GetPkg: (p: string) => { [key: string]: string } = p =>
+  IsPkg(p) ? require(`${p}/package.json`) : {};
