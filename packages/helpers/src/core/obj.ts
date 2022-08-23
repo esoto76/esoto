12e15b69442b3c8394ac77530517c07e4a3abfb7
esoto76 <elpasocolorartist@yahoo.com>
2022-08-22 18:59:46 -0600

    feat(helpers): add ObjDeepMerge and ObjDeepMergeAll

--- packages/helpers/src/core/obj.ts
+++ packages/helpers/src/core/obj.ts
@@ -21,3 +21,43 @@ export const MergeObjs: (v: GEN_OBJ[]) => GEN_OBJ = v => {
 
   return obj;
 };
+
+export const ObjDeepMerge: (x: GEN_OBJ, y: GEN_OBJ) => GEN_OBJ = (x, y) => {
+  if (!IsObj(x) && !IsObj(y)) return {};
+  if (!IsObjDef(y)) return x;
+  if (!IsObjDef(x)) return y;
+
+  for (let k in y) {
+    const _o = x[k];
+    const _n = y[k];
+    if (!IsDef(_o) || (!IsObjDef(_o) && !IsArrDef(_o))) {
+      x[k] = _n;
+      continue;
+    }
+    if (!IsObjDef(_n) && !IsArrDef(_n)) continue;
+    if (IsObjDef(_n) && IsObjDef(_o)) {
+      x[k] = ObjDeepMerge(_o, _n);
+      continue;
+    }
+    if (IsArrDef(_n) && IsArr(_o)) {
+      for (let v of _n) _o.push(v);
+      x[k] = _o;
+      continue;
+    }
+  }
+
+  return x;
+};
+
+export const ObjDeepMergeAll: (arr: GEN_OBJ[]) => GEN_OBJ = arr => {
+  if (!IsArr(arr)) return {};
+  arr = arr.filter(o => IsObjDef(o));
+  if (!IsArrDef(arr)) return {};
+  if (arr.length === 1) return arr[0];
+
+  let obj = {};
+
+  for (let o of arr) if (IsObjDef(o)) obj = ObjDeepMerge(obj, o);
+
+  return obj;
+};
