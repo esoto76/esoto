44d3c28b8df54ca5f05bfcc779ef8e674e8015ad
esoto76 <elpasocolorartist@yahoo.com>
2022-08-22 19:14:33 -0600

    feat(helpers): add ArrMerge

--- packages/helpers/src/core/arr.ts
+++ packages/helpers/src/core/arr.ts
@@ -1,6 +1,16 @@
-import { ISFN } from './types';
-import { IsDef, IsLength } from './misc';
+import { ISFN } from "./types";
+import { IsDef, IsLength } from "./misc";
 
 export const IsArr: ISFN = v => (IsDef(v) && Array.isArray(v) ? true : false);
 
 export const IsArrDef: ISFN = v => (IsArr(v) && IsLength(v) ? true : false);
+
+export const ArrMerge: (x: any[], y: any[]) => any[] = (x, y) => {
+  if (!IsArrDef(x) && !IsArrDef(y)) return [];
+  if (IsArr(x) && !IsArrDef(y)) return x;
+  if (!IsArrDef(x)) return y;
+
+  for (let v of y) x.push(v);
+
+  return x;
+};
